236283220adf3bc4839b7eb19fba210a067aee6e
ヒロタタカユキ <takayukihirota3@gmail.com>
2022-05-08 23:23:05 +0900

    axios

--- memo_app/src/components/HelloWorld.vue
+++ memo_app/src/components/HelloWorld.vue
@@ -1,19 +1,44 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <section class="alert alert-primary">
+    <h1>{{data.title}}</h1>
+    <p>{{data.message}}</p>
+    <table class="table table-light table-striped">
+      <thead class="table-dark text-center">
+        <tr><th>Name</th><th>Mail</th><th>Age</th></tr>
+      </thead>
+      <tbody class="text-left">
+      <tr v-for="(item, key) in data.fire_data">
+        <td>{{item.name}}</td>
+        <td>{{item.age}}</td>
+        <td>{{key}}</td>
+      </tr>
+      </tbody>
+    </table>
+  </section>
 </template>
 
 <script>
+import axios from 'axios'
+import { onMounted, reactive } from 'vue'
+
+let url = "https://vue3-50a9b-default-rtdb.firebaseio.com/person.json" //☆
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  },
-  data() {
-    return {
-      count: 0
+  setup(props) {
+    const data = reactive({
+      title:'Firebase',
+      message:'This is Firebase sample.',
+      fire_data: null,
+    })
+    const getData = ()=> {
+      axios.get(url).then((result)=> {
+        data.fire_data = result.data
+      })
     }
-  }
+    onMounted(()=> {
+      getData()
+    })
+    return { data }
+  },
 }
 </script>
